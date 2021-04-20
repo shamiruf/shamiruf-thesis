6ab20d2a14cdba7e30c74ce7924acb554205e3b2
Rufi <43849574+shamiruf@users.noreply.github.com>
2021-04-20 23:13:13 +0200

    add get request for one tour

--- routes/api/tours.js
+++ routes/api/tours.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const auth = require("../../middleware/auth");
 // Tour model to make queries
 const Tour = require("../../models/tour");
 
@@ -8,14 +8,33 @@ const Tour = require("../../models/tour");
 // @route  GET api/tours
 // @desc   Get All Tours
 // @access Public
-// "/" represents api/tours endpoint
 router.get("/", (req, res) => {
+  // TODO
+  // SORT BY RAITING
   Tour.find().then((tours) => res.json(tours));
+});
+
+// @route  GET api/tours
+// @desc   Get One Tour
+// @access Public
+router.get("/:nameTour", (req, res) => {
+  Tour.findOne({ nameTour: req.params.nameTour }, function (err, tour) {
+    if (err) {
+      console.log(err);
+    }
+    if (!tour) {
+      res.json({ status: "NOT FOUND" });
+    }
+    res.json(tour);
+  });
 });
 
 // @route  POST api/tours
 // @desc   Create A Tour
-// @access Public
+// @access Private
+
+// TODO
+// ADD AUTH like 2 argument
 router.post("/", (req, res) => {
   const waypoints = req.body.waypoints;
   Tour.findOne({ waypoints: waypoints }, function (err, waypoints) {
@@ -29,8 +48,9 @@ router.post("/", (req, res) => {
       // create newTour in memory
       const newTour = new Tour({
         nameTour: req.body.nameTour,
-        // mapsLink: req.body.googleMapsLinkDir,
+        mapsLink: req.body.mapsLink,
         waypoints: req.body.waypoints,
+        tourDescription: req.body.tourDescription,
       });
 
       /// save newTour in db
@@ -45,7 +65,10 @@ router.post("/", (req, res) => {
 
 // @route  DELETE api/tours
 // @desc   DELETE An Tour
-// @access Public
+// @access Private
+
+// TODO
+// ADD AUTH like 2 argument
 router.delete("/:id", (req, res) => {
   Tour.findById(req.params.id)
     .then((tour) => tour.remove().then(() => res.json({ success: true })))
